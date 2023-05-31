b0dae855cd60b3939771a160e67741667597e796
dragonbanana <davideyi.hu@polimi.it>
2023-05-31 20:00:25 +0200

    Neptune Edge Workload

--- dataset.py
+++ dataset.py
@@ -1,20 +1,21 @@
 import pathlib
 import re
-
-import numpy as np
 import pandas as pd
+from sklearn.preprocessing import QuantileTransformer
 
 
-class User:
+class UserFactory:
 
-    def __init__(self):
-        pass
+    def __init__(self, users: pd.DataFrame):
+        self.users = users
 
-    def get_position(self, timestamp: int):
-        pass
+    def get_position(self, timestamp: float):
+        filtered_users = self.users[:self.users['start'].searchsorted(timestamp, side='left')]
+        filtered_users = filtered_users[filtered_users['end'] >= timestamp]
+        return filtered_users
 
 
-class CabspottingUserFactory:
+class CabspottingUserFactory(UserFactory):
 
     def __init__(self, dataset_dir: str):
         self.dataset_dir = pathlib.Path(dataset_dir)
@@ -26,15 +27,27 @@ class CabspottingUserFactory:
             cab_df = pd.read_csv(self.dataset_dir.joinpath(f"new_{cab_id}.txt"), sep=" ", header=None,
                                  names=['lat', 'long', 'state', 'timestamp'], usecols=['lat', 'long', 'timestamp'])
             cab_df['id'] = i
+            cab_df = cab_df.sort_values('timestamp')
+            timestamps = cab_df['timestamp'].to_list()
+            cab_df = cab_df[1:]
+            cab_df['start'] = timestamps[:-1]
+            cab_df['end'] = timestamps[1:]
+            cab_df = cab_df.drop(columns='timestamp')
             cabs_info.append(cab_df)
-        self.cabs = pd.concat(cabs_info)
-        normalized_cabs = (self.cabs - self.cabs.min()) / (self.cabs.max() - self.cabs.min())
-        self.cabs['lat'] = normalized_cabs['lat']
-        self.cabs['long'] = normalized_cabs['long']
-        self.cabs['timestamp'] = normalized_cabs['timestamp']
+        users = pd.concat(cabs_info)
+        # Normalize towards uniform distribution
+        cols = ['lat', 'long']
+        scaler = QuantileTransformer()
+        users[cols] = scaler.fit_transform(users[cols])
+        min_time = users['start'].min()
+        max_time = users['end'].max()
+        for col in ['start', 'end']:
+            users[col] = (users[col] - min_time) / (max_time - min_time)
+        users = users.sort_values(['start', 'end'])
+        super().__init__(users)
 
 
-class TDriveUserFactory:
+class TDriveUserFactory(UserFactory):
 
     def __init__(self, dataset_dir: str):
         self.dataset_dir = pathlib.Path(dataset_dir)
@@ -47,52 +60,64 @@ class TDriveUserFactory:
                                  date_format="%Y-%m-%d %H:%M:%S", names=['timestamp', 'lat', 'long'])
             cab_df.columns = ['timestamp', 'lat', 'long']
             cab_df['id'] = i
-            cab_df['timestamp'] = cab_df['timestamp']
+            # cab_df['timestamp'] = cab_df['timestamp']
+            cab_df = cab_df.sort_values('timestamp')
+            timestamps = cab_df['timestamp'].to_list()
+            cab_df = cab_df[1:]
+            cab_df['start'] = timestamps[:-1]
+            cab_df['end'] = timestamps[1:]
+            cab_df = cab_df.drop(columns='timestamp')
             cabs_info.append(cab_df)
-        self.cabs = pd.concat(cabs_info)
-        normalized_cabs = (self.cabs - self.cabs.min()) / (self.cabs.max() - self.cabs.min())
-        self.cabs['lat'] = normalized_cabs['lat'].astype(float)
-        self.cabs['long'] = normalized_cabs['long'].astype(float)
-        self.cabs['timestamp'] = normalized_cabs['timestamp'].astype(float)
+        users = pd.concat(cabs_info)
+        # Normalize towards uniform distribution
+        cols = ['lat', 'long']
+        scaler = QuantileTransformer()
+        users[cols] = scaler.fit_transform(users[cols])
+        min_time = users['start'].min()
+        max_time = users['end'].max()
+        for col in ['start', 'end']:
+            users[col] = (users[col] - min_time) / (max_time - min_time)
+        users = users.sort_values(['start', 'end'])
+        super().__init__(users)
 
 
-class TelecomUserFactory:
+class TelecomUserFactory(UserFactory):
 
     def __init__(self, dataset_dir: str):
         self.dataset_dir = pathlib.Path(dataset_dir)
-        self.users = pd.read_excel(self.dataset_dir.joinpath("data_6.1~6.15.xlsx"),
-                                  usecols=['start time', 'end time', 'latitude', 'longitude', 'user id'],
-                                  parse_dates=['start time', 'end time'],
-                                  date_format="%Y-%m-%d %H:%M:%S", )
-        self.users = self.users[~(self.users.isna().sum(axis=1).astype(bool))]
-        self.users['user id'] = self.users['user id'].astype('category').cat.codes
-        self.users = self.users.rename(columns={'start time': 'start', 'end time': 'end', 'user id': 'id', 'latitude': 'lat', 'longitude': 'long'})
-        normalized_cabs = (self.users - self.users.min()) / (self.users.max() - self.users.min())
-        self.users['lat'] = normalized_cabs['lat'].astype(float)
-        self.users['long'] = normalized_cabs['long'].astype(float)
-        self.users['start'] = normalized_cabs['start'].astype(float)
-        self.users['end'] = normalized_cabs['end'].astype(float)
-
-        # cabs_info = []
-        # for i in range(1, 10357):
-        #     file = self.dataset_dir.joinpath(f"release/taxi_log_2008_by_id/{i}.txt")
-        #     if file.stat().st_size == 0:
-        #         continue
-        #     cab_df = pd.read_csv(file, sep=",", header=None, index_col=0, parse_dates=['timestamp'],
-        #                          date_format="%Y-%m-%d %H:%M:%S", names=['timestamp', 'lat', 'long'])
-        #     cab_df.columns = ['timestamp', 'lat', 'long']
-        #     cab_df['cab_id'] = i
-        #     cab_df['timestamp'] = cab_df['timestamp']
-        #     cabs_info.append(cab_df[:100])
-        # self.cabs = pd.concat(cabs_info)
-        # normalized_cabs = (self.cabs - self.cabs.min()) / (self.cabs.max() - self.cabs.min())
-        # self.cabs['lat'] = normalized_cabs['lat'].astype(float)
-        # self.cabs['long'] = normalized_cabs['long'].astype(float)
-        # self.cabs['timestamp'] = normalized_cabs['timestamp'].astype(float)
+        users = pd.read_excel(self.dataset_dir.joinpath("data_6.1~6.15.xlsx"),
+                              usecols=['start time', 'end time', 'latitude', 'longitude', 'user id'],
+                              parse_dates=['start time', 'end time'],
+                              date_format="%Y-%m-%d %H:%M:%S", )
+        users = users[~(users.isna().sum(axis=1).astype(bool))]
+        users['user id'] = users['user id'].astype('category').cat.codes
+        users = users.rename(
+            columns={'start time': 'start', 'end time': 'end', 'user id': 'id', 'latitude': 'lat', 'longitude': 'long'})
+        normalized_cabs = (users - users.min()) / (users.max() - users.min())
+        users['lat'] = normalized_cabs['lat'].astype(float)
+        users['long'] = normalized_cabs['long'].astype(float)
+        users['start'] = normalized_cabs['start'].astype(float)
+        users['end'] = normalized_cabs['end'].astype(float)
+        # Normalize towards uniform distribution
+        cols = ['lat', 'long']
+        scaler = QuantileTransformer()
+        users[cols] = scaler.fit_transform(users[cols])
+        users = users.sort_values(['start', 'end'])
+        super().__init__(users)
 
 
 if __name__ == '__main__':
-    # user_factory = CabspottingUserFactory("cabspottingdata")
-    # user_factory = TDriveUserFactory("tdrive")
+    n = 10000
+    user_factory = CabspottingUserFactory("cabspottingdata")
+    for t in range(1, n):
+        users = user_factory.get_position(t/n)
+        assert len(users) > 0
+    user_factory = TDriveUserFactory("tdrive")
+    for t in range(1, n):
+        users = user_factory.get_position(t/n)
+        assert len(users) > 0
     user_factory = TelecomUserFactory("telecom")
+    for t in range(1, n):
+        users = user_factory.get_position(t/n)
+        assert len(users) > 0
     pass
